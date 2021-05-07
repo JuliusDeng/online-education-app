3280be2892c94e805c5b4994252e1f396f5faf4a
JuliusDeng <1576554007@qq.com>
2021-05-07 17:43:32 +0800

    tabbar配置。

---
+++ components/f-search-bar/f-search-bar.vue
@@ -0,0 +1,20 @@
+<template>
+	<view>
+		lalal 
+	</view>
+</template>
+
+<script>
+	export default {
+		name:"f-search-bar",
+		data() {
+			return {
+				
+			};
+		}
+	}
+</script>
+
+<style>
+
+</style>
---
+++ pages/tabbar/index/index.vue
@@ -0,0 +1,31 @@
+<template>
+	<view>
+		
+		<view class="flex justify-center align-center animate__animated animate__bounce">
+			这是文字居中
+		</view>
+		<view class="iconfont icon-wode"  style="font-size: 30px;">
+			
+		</view>
+		
+		<f-search-bar></f-search-bar>
+		
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				
+			}
+		},
+		methods: {
+
+		}
+	}
+</script>
+
+<style>
+	
+</style>
